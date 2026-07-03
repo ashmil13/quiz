bef8945f0f9d91d554ccfc62d8b47a6e85318142
ashmil <ashmil13june@gmail.com>
2026-07-03 12:25:56 +0530

    Configure Vercel deployment and update frontend axios base URL

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+// const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://quiz-ten-delta-13.vercel.app';
 
 export default axios.create({
   baseURL: BASE_URL
